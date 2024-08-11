3a88f841331f354e0b37049ed4c9d18a90f5579c
phillyCHEEEEEZ <panac@nacamuli.net>
2024-08-11 00:41:11 -0500

    update image logic and balloonist icon

--- src/CharacterImage.tsx
+++ src/CharacterImage.tsx
@@ -24,20 +24,27 @@ export function CharacterImage(props: CharacterImageProps) {
   let xlinkHref;
   let x, y, width, height;
 
-  const useOfficialImage = false;
+  const useOfficialImages = false;
+  const useAlternateImages = false;
 
-  if (role?.image && useOfficialImage) { 
+  if (role?.image && useOfficialImages) { 
     xlinkHref = role.image;
     x = -12 + (177 - (iconSize * 4) / 5) / 2;
     y = yOffset + 24 + (124 - (iconSize * 4) / 5) / 2;
     width = (iconSize * 4) / 5;
     height = (iconSize * 4) / 5;
-  } else {
-    xlinkHref = `https://raw.githubusercontent.com/phillyCHEEEEEZ/BotC-Token-Generator/dev/src/icons/${roleId}.png`;
+  } else if (useAlternateImages) {
+    xlinkHref = `https://raw.githubusercontent.com/chizmw/json-on-the-clocktower/main/data/images/${roleId}.png`;
     x = 15 + (120 - (iconSize * 4) / 5) / 2;
     y = yOffset + 34 + (120 - (iconSize * 4) / 5) / 2;
     width = (iconSize * 4) / 5;
     height = (iconSize * 4) / 5;
+  } else {
+    xlinkHref = `https://raw.githubusercontent.com/phillyCHEEEEEZ/BotC-Token-Generator/dev/src/icons/${roleId}.png`;
+    x = 15 + (120 - (iconSize * 4) / 6) / 2;
+    y = yOffset + 26 + (120 - (iconSize * 4) / 6) / 2;
+    width = (iconSize * 4) / 6;
+    height = (iconSize * 4) / 6;
   }
 
   return (
